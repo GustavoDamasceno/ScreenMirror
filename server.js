20791c2b1c8d418c78bc1bb31890f8211d525e4d
Damasceno <eng.damasceno@alu.ufc.br>
2021-11-16 19:51:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
-
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname+'/public/index.html')
+})
 
 io.on('connection', (socket)=>{
     console.log('Novo cliente conectado no id:',socket.id);
